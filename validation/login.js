b9bc01705e8de276c616824ec332239d525d3873
sam <s.gelernter@gmail.com>
2022-01-17 17:38:57 -0500

    Create user validations - not yet implemented in routes

---
+++ validation/login.js
@@ -0,0 +1,30 @@
+// const Validator = require('validator');
+// const validText = require('./valid-text');
+
+// module.exports = function validateLoginInput(data) {
+
+//     let errors = {};
+    
+//     data.email = validText(data.email) ? data.email : '';
+//     data.username = validText(data.username) ? data.username : '';
+//     data.password = validText(data.password) ? data.password : '';
+
+//     if (!Validator.isEmail(data.email)) {
+//         errors.email = 'Invalid email';
+//     }
+
+//     if (Validator.isEmpty(data.password)) {
+//         errors.password = 'You must enter a password';
+//     }
+
+//     if (Validator.isEmpty(data.email) && Validator.isEmpty(data.username)) {
+//         errors.login = 'You must enter a username or email';
+//     }
+
+//     return {
+//         errors,
+//         isValid: Object.keys(errors).length === 0;
+//     }
+// }
+
+// LOGIN NEEDS TO BE FACTORED TO ACCOUNT FOR VARIABLE INPUT
---
+++ validation/register.js
@@ -0,0 +1,48 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateRegisterInput(data) {
+    
+    let errors = {};
+
+    // NEED TO CHECK VARIABLE NAMES FOR CONFIRM PASSWORD
+
+    data.username = validText(data.username) ? data.username : '';
+    data.email = validText(data.email) ? data.email : '';
+    data.password = validText(data.password) ? data.password : '';
+    data.confirmPassword = validText(data.confirmPassword) ? data.confirmPassword : '';
+
+    if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
+        errors.username = 'Username must be between 2 and 30 characters';
+    }
+
+    if (Validator.isEmpty(data.username)) {
+        errors.username = 'Username is required';
+    }
+
+    if (Validator.isEmpty(data.email)) {
+        errors.email = 'Email is required';
+    }
+
+    if (!Validator.isEmail(data.email)) {
+        errors.email = 'Invalid email';
+    }
+
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'Password is required';
+    }
+
+    if (Validator.isEmpty(data.confirmPassword)) {
+        errors.confirmPassword = 'Confirm password is required';
+    }
+
+    if (!Validator.equals(data.password, data.confirmPassword)) {
+        errors.confirmPassword = 'Passwords must match';
+    }
+
+    return {
+        errors, 
+        isValid: Object.keys(errors).length === 0
+    };
+
+}
