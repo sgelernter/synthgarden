2e516c7562515338085c712a04bbdcc49388da49
sam <s.gelernter@gmail.com>
2022-01-17 14:57:43 -0500

    Connect to MongoDB collection

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+mongoose 
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB successfully'))
+    .catch(err => console.log(err));
+
+const users = require('./routes/api/users');
+
 
 app.get('/', (req, res) => {
     res.send('SynthGarden');
@@ -7,4 +17,6 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {console.log(`listening on port ${port}`)});
+app.use('/api/users/', users);
+
+app.listen(port, () => {console.log(`listening on port ${port}`)});  
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,1 +1,9 @@
-const 
+const express = require('express');
+const router = express.Router();
+
+router.get('/test', (req, res) => res.json({ msg: 'USERS ROUTE TEST SUCCESS'}));
+
+
+
+
+module.exports = router;
