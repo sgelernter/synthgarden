f9355a2244cdc07354ba20f3533eddb8f96b2b68
sam <s.gelernter@gmail.com>
2022-01-17 23:02:09 -0500

    Debug User model

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const users = require('./routes/api/users');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 require('./config/passport')(passport);
+
 mongoose 
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log('Connected to MongoDB successfully'))
-    .catch(err => console.log(err));
-    
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log('Connected to MongoDB successfully'))
+.catch(err => console.log(err));
+
 const port = process.env.PORT || 5000;
 
 
-const users = require('./routes/api/users');
 
 
 app.get('/', (req, res) => {
