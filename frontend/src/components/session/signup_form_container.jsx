3b2e0f3243c9c31ebda6ae5949378fe651f80f8b
stellalc7 <stellalc7@gmail.com>
2022-01-17 21:03:32 -0500

    modaling

---
+++ frontend/src/components/session/signup_form_container.jsx
@@ -0,0 +1,28 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { signup } from '../../actions/session_actions';
+import { openModal, closeModal } from '../../actions/modal_actions';
+
+import SessionForm from './session_form';
+
+const mapStateToProps = state => {
+  return {
+    signedIn: state.session.isSignedIn,
+    errors: state.errors.session,
+    formType: 'signup',
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    processForm: (user) => dispatch(signup(user)),
+    otherForm: (
+      <button onClick={() => dispatch(openModal('login'))}>
+        login
+      </button>
+    ),
+    closeModal: () => dispatch(closeModal())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
