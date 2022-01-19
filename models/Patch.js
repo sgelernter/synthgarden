a9a79aed024edda77078f03a365bd2395c5b729a
Emily <myng.emily@gmail.com>
2022-01-19 14:00:57 -0500

    Update patch schema

--- models/Patch.js
+++ models/Patch.js
@@ -10,6 +10,87 @@ const PatchSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'users'
     },
+    oscillator: {
+        osctype: {
+            type: String,
+            required: true
+        },
+        attack: {
+            type: Number,
+            required: true
+        },
+        sustain: {
+            type: Number,
+            required: true
+        },
+        release: {
+            type: Number,
+            required: true
+        }
+    },
+    equalizer: {
+        low: {
+            type: Number,
+            required: true
+        },
+        mid: {
+            type: Number,
+            required: true
+        },
+        high: {
+            type: Number,
+            required: true
+        },
+    },
+    mods: {
+        type: Boolean,
+        required: true    
+    },
+    chorus: {
+        type: Number
+    },
+    tremelo: {
+        type: Number
+    },
+    distortion: {
+        amount: {
+            type: Number
+        }
+    },
+    bitCrusher: {
+        bitDepth: {
+            type: Number
+        },
+        amount: {
+            type: Number
+        }
+    },
+    delay: {
+        type: Boolean,
+        required: true
+    },
+    feedback: {
+        time: {
+            type: Number
+        },
+        fb: {
+            type: Number
+        },
+        amt: {
+            type: Number
+        }
+    },
+    pingPong: {
+        time: {
+            type: Number
+        },
+        fb: {
+            type: Number
+        },
+        amt: {
+            type: Number
+        }
+    },
     date: {
         type: Date,
         default: Date.now
