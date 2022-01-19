ab89af1ed4eb9a42a429a5a5e6d35a9957497623
stellalc7 <stellalc7@gmail.com>
2022-01-19 14:37:32 -0500

    implementing start/stop recording button flipping logic

--- frontend/src/components/controls/record.jsx
+++ frontend/src/components/controls/record.jsx
@@ -5,33 +5,52 @@ class Record extends React.Component {
   constructor(props) {
     super(props)
     const synth = new Tone.Synth();
+    this.state = {
+      recorder: new Tone.Recorder()
+    }
+    // const recorder = new Tone.Recorder();
     const actx = Tone.context;
     console.log(actx);
     
-    synth.toMaster();
+    synth.toDestination();
 
     const notes = 'CDEFGAB'.split('').map(n => `${n}4`);
     let note = 0;
     Tone.Transport.scheduleRepeat(time => {
-      synth.triggerAttack(notes[note % notes.length]);
+      if (note === 0) {
+        // START RECORDING
+      }
+      if (note > notes.length) {
+        // STOP RECORDING
+
+        // trigger release on synth to stop making noise
+        synth.triggerRelease(time)
+
+        // stop the synth
+        Tone.Transport.stop();
+      } else synth.triggerAttack(notes[note], time);
       note ++;
     }, '4n');
-    Tone.Transport.start();
+    // Tone.Transport.start();
+  }
+
+  startRecording() {
+    this.state.recorder.start();
+  }
+
+  stopRecording() {
+    this.state.recorder.stop();
   }
 
   render() {
+    // SWITCH RECORDING BUTTON - START / STOP
+
+
     return (
       <>
         <audio controls></audio>
-
-        {/* start recording
-            stop recording
-            DELETE?
-
-            download
-            play
-            pause
-            stop */}
+        <button onClick={this.startRecording}>START</button>
+        <button onClick={this.stopRecording}>STOP</button>
       </>
     )
   }
