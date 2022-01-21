4041d7f92f286215ec4268694df2a6d78f93babc
Emily <myng.emily@gmail.com>
2022-01-20 22:53:43 -0500

    Remove commented out stuff from app.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,22 +4,17 @@ import Splash from './splash/splash';
 import { Route, Switch } from 'react-router-dom';
 import NavbarContainer from './navbar/navbar_container';
 import Footer from './footer/footer';
-import { AuthRoute, ProtectedRoute } from '../util/route_utils';
-import SynthstrumentContainer from './synthstrument/synthstrument_container';
+import { ProtectedRoute } from '../util/route_utils';
 import SidebarContainer from './sidebar/sidebar_container';
-// import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
     <header>
       <NavbarContainer />
     </header>
-    {/* <Switch> */}
-      {/* <Route path='/' component={SynthstrumentContainer} /> */}
       <Modal />  
       <ProtectedRoute path='/' component={SidebarContainer} />
       <Route path="/" component={Splash} />
-    {/* </Switch> */}
     <footer>
       <Footer />
     </footer>
