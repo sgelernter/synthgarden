ad867523d0873d261b542f666c998f6fd3c50ec4
Emily <myng.emily@gmail.com>
2022-01-20 22:38:35 -0500

    Add styling to navbar

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,18 +1,29 @@
 import React from "react";
-// import NavbarContainer from "./navbar/navbar_container";
 import Modal from './modal/modal';
 import Splash from './splash/splash';
 import { Route, Switch } from 'react-router-dom';
+import NavbarContainer from './navbar/navbar_container';
+import Footer from './footer/footer';
+import { AuthRoute, ProtectedRoute } from '../util/route_utils';
+import SynthstrumentContainer from './synthstrument/synthstrument_container';
+import SidebarContainer from './sidebar/sidebar_container';
 // import { Route } from "react-router-dom";
-
 
 const App = () => (
   <div>
-    <Modal />  
-    <Route exact path="/" component={Splash} />
-    {/* <Splash /> */}
+    <header>
+      <NavbarContainer />
+    </header>
+    {/* <Switch> */}
+      {/* <Route path='/' component={SynthstrumentContainer} /> */}
+      <Modal />  
+      <ProtectedRoute path='/' component={SidebarContainer} />
+      <Route path="/" component={Splash} />
+    {/* </Switch> */}
+    <footer>
+      <Footer />
+    </footer>
   </div>
-
 );
 
 export default App;
--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import Navbar from '../navbar/navbar';
 import NavbarContainer from '../navbar/navbar_container';
 // import Record from '../synthstrument/record';
-// import { AuthRoute } from '../../util/route_utils';
+import { AuthRoute } from '../../util/route_utils';
 // import Synthstrument from '../synthstrument/synthstrument';
 import SynthstrumentContainer from '../synthstrument/synthstrument_container';
 import SidebarContainer from '../sidebar/sidebar_container';
@@ -11,8 +11,8 @@ import '../../assets/stylesheets/splash.scss';
 export default function Splash() {
     return (
         <div className="splash-container">
-            <NavbarContainer />
-            < SidebarContainer />
+            {/* <NavbarContainer />
+            < SidebarContainer /> */}
             <SynthstrumentContainer />
         </div>
     )
