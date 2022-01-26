e317f0a9bb650c16a1b2da90df5b5ab90bcc58d0
stellalc7 <stellalc7@gmail.com>
2022-01-26 18:55:15 -0500

    instruction modal copy in progress - team discussion

--- frontend/src/components/instructions/instructions.jsx
+++ frontend/src/components/instructions/instructions.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import '../../assets/stylesheets/instructions.scss';
 
 class Instructions extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+    // constructor(props) {
+    //     super(props);
+    // }
 
     render() {
         return (
-            <div>
+            <div className="instructions">
                 <div onClick={this.props.closeModal} className="close-x">x</div>
-
+                Welcome to SynthGarden!<br></br>
+                Play with an in-browser synth instrument. Press [POWER], and explore various synth settings, and create your own patches! Record your own samples with various patch settings, and download them for later!
             </div>
         )
     }
