42e657bcb0e3eb6fa236162d3fc121019f7cb306
Emily <myng.emily@gmail.com>
2022-01-31 01:18:40 -0500

    Add styling to instructions modal and fixed volume

--- frontend/src/components/instructions/instructions.jsx
+++ frontend/src/components/instructions/instructions.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import '../../assets/stylesheets/instructions.scss';
 
 class Instructions extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
 
     render() {
         return (
             <div className="instructions">
                 <div onClick={this.props.closeModal} className="close-x">x</div>
-                Welcome to SynthGarden!<br></br>
-                Play with an in-browser synth instrument. Press [POWER], and explore various synth settings, and create your own patches! Record your own samples with various patch settings, and download them for later!
+                <div>
+                    <p>Welcome to</p>
+                    <h1>SynthGarden</h1>
+                    <p>Play with an in-browser synth instrument. Press [POWER], and explore various synth settings, and create your own patches! Record your own samples with various patch settings, and download them for later!</p>
+                </div>
             </div>
         )
     }
