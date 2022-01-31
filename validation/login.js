3e79ca3470b6e1388e52ddb6e0f8dfc00d863f5b
Emily <myng.emily@gmail.com>
2022-01-30 20:53:04 -0500

    Comment in login validations

--- validation/login.js
+++ validation/login.js
@@ -1,30 +1,30 @@
-// const Validator = require('validator');
-// const validText = require('./valid-text');
+const Validator = require('validator');
+const validText = require('./valid-text');
 
-// module.exports = function validateLoginInput(data) {
+module.exports = function validateLoginInput(data) {
 
-//     let errors = {};
+    let errors = {};
     
-//     data.email = validText(data.email) ? data.email : '';
-//     data.username = validText(data.username) ? data.username : '';
-//     data.password = validText(data.password) ? data.password : '';
+    data.email = validText(data.email) ? data.email : '';
+    data.username = validText(data.username) ? data.username : '';
+    data.password = validText(data.password) ? data.password : '';
 
-//     if (!Validator.isEmail(data.email)) {
-//         errors.email = 'Invalid email';
-//     }
+    if (!Validator.isEmail(data.email)) {
+        errors.email = 'Invalid email';
+    }
 
-//     if (Validator.isEmpty(data.password)) {
-//         errors.password = 'You must enter a password';
-//     }
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'You must enter a password';
+    }
 
-//     if (Validator.isEmpty(data.email) && Validator.isEmpty(data.username)) {
-//         errors.login = 'You must enter a username or email';
-//     }
+    if (Validator.isEmpty(data.email) && Validator.isEmpty(data.username)) {
+        errors.login = 'You must enter a username or email';
+    }
 
-//     return {
-//         errors,
-//         isValid: Object.keys(errors).length === 0;
-//     }
-// }
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    }
+}
 
 // LOGIN NEEDS TO BE FACTORED TO ACCOUNT FOR VARIABLE INPUT
