195ecaae045bfa44da9a29a6c806f5a208ae2f77
sam <s.gelernter@gmail.com>
2022-01-31 22:23:22 -0500

    Add oscilloscope

---
+++ frontend/src/components/synthstrument/oscilloscope.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Oscilloscope, getUserMedia } from "webaudio-oscilloscope";
+import * as Tone from "tone";
+
+class Scope extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidUpdate(prev){
+        if (this.props.contextStarted !== prev.contextStarted && this.props.contextStarted === 'true') {
+            const context = Tone.getContext();
+            const canvas = document.getElementById('oscilloscope');
+            this.gainNode = new Tone.Gain(2);
+            this.props.lastLink.connect(this.gainNode);
+            this.osc = new Oscilloscope(context, this.gainNode, canvas);
+            const canvasContext = this.osc.cctx;
+            canvasContext.shadowBlur = 8;
+            canvasContext.lineWidth = 1.5;
+            canvasContext.strokeStyle = '#c4ffc4';
+            canvasContext.shadowColor = '#229e15';
+            canvasContext.globalAlpha = 1.5;
+            this.osc.start();
+        }
+        if (this.props.lastLink !== prev.lastLink) {
+            prev.lastLink.disconnect(this.gainNode);
+            this.props.lastLink.connect(this.gainNode);
+        }
+    }
+    
+    render(){
+        return (
+            <div className="oscilloscope-container">
+                <canvas id="oscilloscope" width="150px" height="150px"/>
+            </div>
+        )
+    }
+}
+
+
+export default Scope;
+
